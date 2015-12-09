be996a778910890139287164ec4c1b9b5648d03d
Marcus Nyeholt <marcus@silverstripe.com.au>
2015-12-09 17:33:11 +1100

    NEW Added controller to handle direct requests
    For testing / debugging purposes by logged in users, allows the loading of
    stage=Stage data

---
+++ code/controllers/ScriptGenieController.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * serves generated JS files 
+ *
+ * @author marcus
+ */
+class ScriptGenieController extends Controller {
+	
+	private static $allowed_actions = array(
+		'script'
+	);
+	
+	private static $dependencies = array(
+		'scriptService'		=> '%$GenieScriptService'
+	);
+	
+	/**
+	 * @var GenieScriptService
+	 */
+	public $scriptService;
+	
+	public function script() {
+		// we don't allow access unless the user is logged in 
+		if (!Member::currentUserID()) {
+			return;
+		}
+		
+		$type = $this->request->param('ID');
+		$filename = $this->request->param('OtherID');
+		
+		$content = $this->scriptService->generateScriptDataFor($type, $filename, Versioned::current_archived_date());
+		
+		$this->response->addHeader('Content-type', 'text/javascript');
+		
+		return $content;
+	}
+}
